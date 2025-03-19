d9c0b9101ffc618aeba7fe4306779b8df4c1e93c
Patricio Guzman <pguzmanosorio@gmail.com>
2025-03-19 09:22:16 +0100

    Initial Commit

---
+++ Lumiform/LumiformApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumiformApp.swift
+//  Lumiform
+//
+//  Created by Patricio Guzman on 19/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct LumiformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LumiformTests/LumiformTests.swift
@@ -0,0 +1,17 @@
+//
+//  LumiformTests.swift
+//  LumiformTests
+//
+//  Created by Patricio Guzman on 19/03/2025.
+//
+
+import Testing
+@testable import Lumiform
+
+struct LumiformTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
